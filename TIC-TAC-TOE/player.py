ad3c8dc8f1fead786574a3983cf1cefdcb9c41fb
Ikiliagwu Chukwudi <57571996+IkiliagwuC@users.noreply.github.com>
2023-01-02 23:14:31 +0100

    Add files via upload

---
+++ TIC-TAC-TOE/player.py
@@ -0,0 +1,38 @@
+#import important modules
+import math
+import random
+from zoneinfo import available_timezones
+
+class Player:
+    def __init__(self, letter):
+        #letter is an x or o
+        self.letter = letter
+    
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self,letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        self.letter = letter
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+            except ValueError:
+                print('Invalid square. Try again.')
+
+        return val
